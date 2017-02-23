9ac93cb07e3ebc7384efaf122a31dab1368bd6c5
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-02-23 01:24:13 -0800

    Initial handling of a list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,40 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Mow the fence'
+        },
+        {
+          id: 4,
+          text: 'Eat the turd'
+        },
+        {
+          id: 5,
+          text: 'Feed the fish'
+        }
+      ]
+    };
+  },
+
   render: function() {
+    var {todos} = this.state;
+
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
