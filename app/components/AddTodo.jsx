0a9e246989208ffa821d35cd149288757837c9ab
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-02-23 13:40:11 -0800

    Add add todo form.

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  onAddTodo: function(e) {
+    e.preventDefault();
+    var todoText = this.refs.todoText.value;
+    if(todoText.length) {
+      this.refs.todoText.value = '';
+      this.props.onAddTodo(todoText);
+    } else {
+      this.refs.todoText.focus();
+      alert('Please enter some text before submitting');
+    }
+  },
+
+  render: function() {
+    return (
+      <div>
+        <form onSubmit={this.onAddTodo}>
+          <input type="text" ref="todoText" placeholder="What do you need to do" />
+          <button className="button expanded">Add Todo</button>
+        </form>
+      </div>
+    )
+  }
+})
+
+module.exports = AddTodo;
