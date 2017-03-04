96a9ecf6b54b565c211bb5113a5620807c151b14
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-03-04 00:37:25 -0800

    cleaned up completed date and boolean, plus added tests for same

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -14,16 +14,22 @@ describe('TodoApp', () => {
   it('should add todo to the todos state on handleAddTodo', () => {
     var todoText = 'test text';
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+
     todoApp.setState({todos: []});
     todoApp.handleAddTodo(todoText);
+
     expect(todoApp.state.todos[0].text).toBe(todoText);
+
+    expect(todoApp.state.todos[0].createdAt).toBeA('number')
   })
 
   it('should toggle completed value when toggle called', () => {
     var todoData = {
       id: 11,
       text: 'test text',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     }
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
     todoApp.setState({
@@ -31,7 +37,32 @@ describe('TodoApp', () => {
     });
 
     expect(todoApp.state.todos[0].completed).toBe(false);
+
     todoApp.handleToggle(11);
     expect(todoApp.state.todos[0].completed).toBe(true);
+
+    expect(todoApp.state.todos[0].completedAt).toBeA('number')
+  })
+
+  // Test when we toggle from true to false, completedAt get removed
+  it('should remove completedAt value when complete is unset', () => {
+    var todoData = {
+      id: 11,
+      text: 'test text',
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
+    }
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+    todoApp.setState({
+      todos: [todoData]
+    });
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   })
 })
