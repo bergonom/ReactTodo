0f3116c451436a14fafe92ad1828b3067dbb9a07
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-03-05 00:26:23 -0800

    Style todo application

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,9 +5,15 @@ var TodoList = React.createClass({
   render: function() {
     var {todos, onToggle} = this.props;
     var renderTodos = () => {
-      return todos.map((todo) => {
-        return <Todo key={todo.id} {...todo} onToggle={onToggle} />
-      })
+      if(todos.length) {
+        return todos.map((todo) => {
+          return <Todo key={todo.id} {...todo} onToggle={onToggle} />
+        })
+      } else {
+        return (
+          <p className="container__message">Nothing to do</p>
+        )
+      }
     };
 
     return (
