2f37d31ff62b63787a1528a010ea2991c35f7ad6
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-03-31 09:55:20 -0700

    Add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state = '', action) => {
   switch(action.type) {
     case 'SET_SEARCH_TEXT':
@@ -16,4 +19,35 @@ export var showCompletedReducer = (state = false, action) => {
   };
 };
 
-// , default false,
+export var todosReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          completedAt: undefined,
+          createdAt: moment().unix()
+        }
+      ];
+
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if(todo.id === action.id) {
+          var nextCompleted = !todo.completed;
+          return  {
+                    ...todo,
+                    completed: nextCompleted,
+                    completedAt: nextCompleted ? moment().unix() : undefined
+                  }
+        } else {
+          return todo;
+        }
+      });
+
+    default:
+      return state;
+  };
+}
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,8 @@
 var expect = require('expect');
 var reducers = require('reducers');
+
+// Deep Freeze makes sure we don't modify passed objects within reducers
+// Reducers are supposed to be pure functions (which don't change passed objects)
 var df = require('deep-freeze-strict');
 
 describe('Reducers', () => {
@@ -26,5 +29,44 @@ describe('Reducers', () => {
 
       expect(result).toEqual(false);
     });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        text: 'Walk the dog',
+        type: 'ADD_TODO'
+      };
+
+      var result = reducers.todosReducer(df([]), df(action));
+
+      expect(result.length).toEqual(1);
+      expect(result[0].text).toEqual(action.text);
+    });
+
+    it('should toggle completed state of todo item', () => {
+      var todos = [
+        {
+          id: 1,
+          text: "wake up",
+          completed: false,
+          completedAt: undefined,
+          createdAt: 1490978283
+        }
+      ];
+
+      var action = {
+        id: 1,
+        type: 'TOGGLE_TODO'
+      };
+
+      var result = reducers.todosReducer(df(todos), df(action));
+
+      expect(result[0].completed).toEqual(true);
+      expect(result[0].completedAt).toExist()
+    });
+    // Define todos array with realistic todo items
+    // generate action
+    // call reduce action and confirm flip
   });
 });
